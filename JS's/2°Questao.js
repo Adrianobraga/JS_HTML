d4f02ea77935256d2eaf17f48538152a5fc9fb74
Adriano Braga <adrianobragamathias007@gmail.com>
2024-09-19 13:22:41 -0300

    Feito

---
+++ JS's/2°Questao.js
@@ -0,0 +1,10 @@
+const valor = document.querySelector('#valor');
+const botao = document.querySelector('#Enviar');
+let resposta = document.querySelector('#Resposta');
+botao.onclick = function(){
+    if(Number.parseInt(valor.value) > 10){
+        resposta.value = "É MAIOR QUE 10!";
+    }else{
+        resposta.value = "NÃO É MAIOR QUE 10!";
+        }
+}
