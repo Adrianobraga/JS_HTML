6f6bc66d27527c32bb04a605020651642cf9d516
Adriano Braga <adrianobragamathias007@gmail.com>
2024-09-19 15:43:24 -0300

    Feito

---
+++ JS's/4°Questão.js
@@ -0,0 +1,15 @@
+let Quantidade = document.querySelector('#quantidade');
+let botao = document.querySelector('#botao');
+let resposta = document.querySelector('#resposta')
+
+
+function Preco (Quantidade){
+    botao.onclick = function(){
+    if(Number.parseInt(Quantidade.value) >= 12){
+         resposta.value = "As maçãs custam R$ 1,90" 
+    }else{
+         resposta.value = "As maçãs custam R$ 2,50" 
+    } 
+}
+}
+Preco(Quantidade);
