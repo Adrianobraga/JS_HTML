9b6e3efbb5614cb775fa8b9c6b6136ceb5aaf16b
Adriano Braga <adrianobragamathias007@gmail.com>
2024-09-24 15:17:00 -0300

    Feito

--- JS's/10Questao.js
+++ JS's/10Questao.js
@@ -1,9 +1,42 @@
-let TotalDeHoras =  160;
-let SalarioPHora = 4.88;
-let HorasTrabalhadas = 160;
-let Somatorio = SalarioPHora * TotalDeHoras;
+
+let botao = document.querySelector("#botao");
+let resposta = document.querySelector("#resposta");
+
+
+  /*
+     @method:salario.
+     @return: string,float;
+     @args: 
+      HorasTrabalhadas: float ,
+      SalarioPHora: float,
+      const TotalDeHoras: 160,
+      Somatorio: float,
+     Verifica as horas trabalhadas por um funciorario
+     e conferir se passou da hora máxima de um mês 
+     se sim, ganha o salario mais 50% 
+    
+ */
+
+const salario = (HorasTrabalhadas,SalarioPHora) => {
+    const TotalDeHoras =  160;
+    const Somatorio = SalarioPHora * HorasTrabalhadas;
+
 if(HorasTrabalhadas <= TotalDeHoras){
-    console.log("O salário recebido no final do mês foi de",Somatorio);
+    resposta.innerHTML = `
+    <p>O salário recebido no final do mês foi de:${Somatorio}</p>
+    `
 }else{
-    console.log("O salário recebido no final do mês foi de",Somatorio*1.5);
+    resposta.innerHTML = `
+    <p>O salário recebido no final do mês foi de:${Somatorio*1.5}</p>
+    `
+    
+}
+}
+
+botao.onclick = function(){
+    const SalarioPHora = Number.parseFloat(document.querySelector("#SalarioPHora").value);
+    const HorasTrabalhadas = Number.parseFloat(document.querySelector("#HorasTrabalhadas").value);
+  
+    salario(HorasTrabalhadas,SalarioPHora)
+  
 }
