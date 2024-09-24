7a345b3930adf8cdbe341933acae3f8f78eabe5a
Adriano Braga <adrianobragamathias007@gmail.com>
2024-09-24 16:43:08 -0300

    Incompleto

---
+++ JS's/15Questao.js
@@ -0,0 +1,25 @@
+let botao = document.querySelector("#botao");
+let resposta = document.querySelector("#resposta"); 
+
+const valores = (x,y)  => {
+
+let z = (x*y)+5
+if (z <= 0){
+let resultado = "A";
+resposta.innerHTML ="A";
+}
+else if( z <= 100){
+resultado = "B"
+resposta.innerHTML ="B";
+}
+else{
+resultado = "C"
+resposta.innerHTML = `Resultado final: , ${z} , Resposta:  , ${resultado} `
+}
+}
+botao.onclick = function (){
+let x = Number.parseInt(document.querySelector("#x").value);
+let y = Number.parseInt(document.querySelector("#y").value);
+console.log(x)
+valores(x,y);
+}
---
+++ JS's/17Questao.js
@@ -0,0 +1,17 @@
+let botao = document.querySelector("#botao");
+let resposta = document.querySelector("#resposta"); 
+
+let CodigoDoUsuario = 1234;
+let SenhaDoUsuario = 9999;
+let CodigoArmazenado = 1234;
+let SenhaArmazenada = 9999;
+if(CodigoDoUsuario == CodigoArmazenado){
+ if(SenhaDoUsuario == SenhaArmazenada){
+    console.log("Acesso permitido");
+ }else{
+    console.log("‘senha incorreta");
+ }   
+
+}else{
+    console.log("Usuário inválido!")
+}
